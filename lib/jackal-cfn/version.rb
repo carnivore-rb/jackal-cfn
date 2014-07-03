9b24a1a0d673664fa3a78b37a765c68bca812f3b
Chris Roberts <chrisroberts.code@gmail.com>
2014-07-03 13:57:13 -0700

    Update version class

--- lib/jackal-cfn/version.rb
+++ lib/jackal-cfn/version.rb
@@ -1,9 +1,6 @@
 module Jackal
   module Cfn
-    # Versioning class
-    class Version < Gem::Version
-    end
     # Current version
-    VERSION = Version.new('0.1.0')
+    VERSION = Gem::Version.new('0.1.0')
   end
 end
