f839a43127c1e33a6ece42650bb691d146b8a2fd
Chris Roberts <chrisroberts.code@gmail.com>
2014-07-03 20:24:00 -0700

    Update for release v0.1.2

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# v0.1.2
+* Include request id in response content
+* Allow matching on class name to custom resource type
+
+# v0.1.0
+* Initial commit
--- lib/jackal-cfn/version.rb
+++ lib/jackal-cfn/version.rb
@@ -1,6 +1,6 @@
 module Jackal
   module Cfn
     # Current version
-    VERSION = Gem::Version.new('0.1.1')
+    VERSION = Gem::Version.new('0.1.2')
   end
 end
