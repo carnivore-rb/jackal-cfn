0c7c5a0ce0aa4a0da6e60cb5da70eeb5b4f0aca1
Chris Roberts <chrisroberts.code@gmail.com>
2014-08-14 14:47:43 -0700

    Update namespace for builtin custom resources

--- lib/jackal-cfn/resource/ami_manager.rb
+++ lib/jackal-cfn/resource/ami_manager.rb
@@ -1,113 +1,111 @@
 require 'jackal-cfn'
 
 module Jackal
-  module Cfn
-    class Resource
-      # Manage AMI Resources
+  module CfnTools
+    # Manage AMI Resources
+    #
+    # Expected resource:
+    #   {
+    #     "Type": "Custom::AmiManager",
+    #     "Properties": {
+    #       "Parameters": {
+    #         "InstanceId": "",
+    #         "AmiId": "",
+    #         "Action": "",
+    #         "Region": ""
+    #       }
+    #     }
+    #   }
+    #
+    # Required configuration:
+    #   {
+    #     "config": {
+    #       "ami": {
+    #         "credentials": {
+    #           "compute": {
+    #             FOG_CREDENTIALS
+    #           }
+    #         }
+    #       }
+    #     }
+    #   }
+    class AmiManager < Jackal::Cfn::Resource
+
+      PHYSICAL_ID_JOINER = '__-__'
+
+      # Process message, send value back to CFN
       #
-      # Expected resource:
-      #   {
-      #     "Type": "Custom::AmiManager",
-      #     "Properties": {
-      #       "Parameters": {
-      #         "InstanceId": "",
-      #         "AmiId": "",
-      #         "Action": "",
-      #         "Region": ""
-      #       }
-      #     }
-      #   }
-      #
-      # Required configuration:
-      #   {
-      #     "config": {
-      #       "ami": {
-      #         "credentials": {
-      #           "compute": {
-      #             FOG_CREDENTIALS
-      #           }
-      #         }
-      #       }
-      #     }
-      #   }
-      class AmiManager < Resource
-
-        PHYSICAL_ID_JOINER = '__-__'
-
-        # Process message, send value back to CFN
-        #
-        # @param message [Carnivore::Message]
-        def execute(message)
-          failure_wrap do |payload|
-            cfn_resource = rekey_hash(payload.get(:data, :cfn_resource))
-            properties = rekey_hash(cfn_resource[:resource_properties])
-            parameters = rekey_hash(properties[:parameters])
-            cfn_response = build_response(cfn_resource)
-            case cfn_resource[:request_type]
-            when :create
-              ami_response_update(cfn_response, parameters)
-            when :delete
-              destroy_ami(cfn_response, cfn_resource, parameters)
-            when :update
-            else
-              error "Unknown request type received: #{cfn_resource[:request_type].inspect}"
-              response['Status'] = 'FAILED'
-              response['Reason'] = 'Unknown request type received'
-            end
-            respond_to_stack(response, cfn_resource[:response_url])
-            completed(payload, message)
-          end
-        end
-
-        # Update the physical resource id to include the ami id
-        #
-        # @param response [Hash] cfn response
-        # @param parameters [Hash] resource parameters
-        # @return [Hash] updated response hash
-        def ami_response_update(response, parameters)
-          response['PhysicalResourceId'] = [
-            response['PhysicalResourceId'],
-            parameters[:ami_id]
-          ].join(PHYSICAL_ID_JOINER)
-          response
-        end
-
-        # Destroy the AMI referenced by the resource
-        #
-        # @param response [Hash] cfn response
-        # @param payload [Hash] message payload
-        # @param parameters [Hash] resource parameters
-        # @return [TrueClass]
-        def destroy_ami(response, payload, parameters)
-          ami_id = payload[:physical_resource_id].split(PHYSICAL_ID_JOINER).last
-          begin
-            compute_api(parameters[:region]).deregister_image(ami_id)
-          rescue Fog::Compute::AWS::Error => e
-            warn "Non-fatal error encountered on AMI removal: #{e.class}: #{e}"
-            response['Reason'] = e.message
-          rescue => e
-            error "Failed to remove AMI: #{e.class}: #{e}"
+      # @param message [Carnivore::Message]
+      def execute(message)
+        failure_wrap do |payload|
+          cfn_resource = rekey_hash(payload.get(:data, :cfn_resource))
+          properties = rekey_hash(cfn_resource[:resource_properties])
+          parameters = rekey_hash(properties[:parameters])
+          cfn_response = build_response(cfn_resource)
+          case cfn_resource[:request_type]
+          when :create
+            ami_response_update(cfn_response, parameters)
+          when :delete
+            destroy_ami(cfn_response, cfn_resource, parameters)
+          when :update
+          else
+            error "Unknown request type received: #{cfn_resource[:request_type].inspect}"
             response['Status'] = 'FAILED'
-            response['Reason'] = e.message
+            response['Reason'] = 'Unknown request type received'
           end
-          true
+          respond_to_stack(cfn_response, cfn_resource[:response_url])
+          completed(payload, message)
         end
+      end
 
-        # Build new compute api connection
-        #
-        # @param region [String] AWS region ami exists
-        # @return [Fog::Compute]
-        def compute_api(region)
-          Fog::Compute.new(
-            {:provider => :aws}.merge(
-              config.fetch(:ami, :credentials, :compute).merge(
-                :region => region
-              )
+      # Update the physical resource id to include the ami id
+      #
+      # @param response [Hash] cfn response
+      # @param parameters [Hash] resource parameters
+      # @return [Hash] updated response hash
+      def ami_response_update(response, parameters)
+        response['PhysicalResourceId'] = [
+          response['PhysicalResourceId'],
+          parameters[:ami_id]
+        ].join(PHYSICAL_ID_JOINER)
+        response
+      end
+
+      # Destroy the AMI referenced by the resource
+      #
+      # @param response [Hash] cfn response
+      # @param payload [Hash] message payload
+      # @param parameters [Hash] resource parameters
+      # @return [TrueClass]
+      def destroy_ami(response, payload, parameters)
+        ami_id = payload[:physical_resource_id].split(PHYSICAL_ID_JOINER).last
+        begin
+          compute_api(parameters[:region]).deregister_image(ami_id)
+        rescue Fog::Compute::AWS::Error => e
+          warn "Non-fatal error encountered on AMI removal: #{e.class}: #{e}"
+          response['Reason'] = e.message
+        rescue => e
+          error "Failed to remove AMI: #{e.class}: #{e}"
+          response['Status'] = 'FAILED'
+          response['Reason'] = e.message
+        end
+        true
+      end
+
+      # Build new compute api connection
+      #
+      # @param region [String] AWS region ami exists
+      # @return [Fog::Compute]
+      def compute_api(region)
+        Fog::Compute.new(
+          {:provider => :aws}.merge(
+            config.fetch(:ami, :credentials, :compute).merge(
+              :region => region
             )
           )
-        end
-
+        )
       end
+
     end
   end
 end
--- lib/jackal-cfn/resource/hash_extractor.rb
+++ lib/jackal-cfn/resource/hash_extractor.rb
@@ -1,48 +1,46 @@
 require 'jackal-cfn'
 
 module Jackal
-  module Cfn
-    class Resource
-      # Extract value from hash
+  module CfnTools
+    # Extract value from hash
+    #
+    # Expected resource:
+    #   {
+    #     "Type": "Custom::HashExtractor",
+    #     "Properties": {
+    #       "Parameters": {
+    #         "Key": "path.to.value.in.hash",
+    #         "Value": Hash_or_JSON_string
+    #       }
+    #     }
+    #   }
+    class HashExtractor < Jackal::Cfn::Resource
+
+      # Process message, send value back to CFN
       #
-      # Expected resource:
-      #   {
-      #     "Type": "Custom::HashExtractor",
-      #     "Properties": {
-      #       "Parameters": {
-      #         "Key": "path.to.value.in.hash",
-      #         "Value": Hash_or_JSON_string
-      #       }
-      #     }
-      #   }
-      class HashExtractor < Resource
-
-        # Process message, send value back to CFN
-        #
-        # @param message [Carnivore::Message]
-        def execute(message)
-          failure_wrap do |payload|
-            cfn_resource = rekey_hash(payload.get(:data, :cfn_resource))
-            properties = rekey_hash(cfn_resource[:resource_properties])
-            parameters = rekey_hash(properties[:parameters])
-            cfn_response = build_response(cfn_resource)
-            key = parameters[:key].split('.')
-            value = parameters[:value]
-            unless(value.is_a?(String))
-              raise TypeError.new("Expecting `String` value but received `#{value.class}`")
-            end
-            value = MultiJson.load(value).to_smash
-            return_value = value.get(*key)
-            if(return_value.is_a?(Enumerable))
-              return_value = MultiJson.dump(return_value)
-            end
-            cfn_response['Data']['Payload'] = return_value
-            respond_to_stack(cfn_response, payload[:response_url])
-            completed(payload, message)
+      # @param message [Carnivore::Message]
+      def execute(message)
+        failure_wrap do |payload|
+          cfn_resource = rekey_hash(payload.get(:data, :cfn_resource))
+          properties = rekey_hash(cfn_resource[:resource_properties])
+          parameters = rekey_hash(properties[:parameters])
+          cfn_response = build_response(cfn_resource)
+          key = parameters[:key].split('.')
+          value = parameters[:value]
+          unless(value.is_a?(String))
+            raise TypeError.new("Expecting `String` value but received `#{value.class}`")
           end
+          value = MultiJson.load(value).to_smash
+          return_value = value.get(*key)
+          if(return_value.is_a?(Enumerable))
+            return_value = MultiJson.dump(return_value)
+          end
+          cfn_response['Data']['Payload'] = return_value
+          respond_to_stack(cfn_response, payload[:response_url])
+          completed(payload, message)
         end
-
       end
+
     end
   end
 end
